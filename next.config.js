21913a8116da5bba8be74fd4760499b5a76ed624
tranbakhanhtrinh <tranbakhanhtrinh@gmail.com>
2023-11-09 13:33:44 +0700

    basepath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
+  basePath: isProd ? '/currency-convert' : '',
   output: 'export',
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
